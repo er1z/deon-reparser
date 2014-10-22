464ad7ce939f469144b28749cdf87c9c64d8f4ed
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2014-10-22 17:38:59 +0200

    something working

--- index.js
+++ index.js
@@ -7,57 +7,82 @@ var express = require('express'),
 
 var router = express.Router();
 
+var q = require('q');
+
 /*var port = require('./package.json').options.port;
 
-app.use(router);
+ app.use(router);
 
-app.get('/reparse', function(req,res){
+ app.get('/reparse', function(req,res){
 
 
-    if(!req.query.id){
-        res.status(404);
-        return;
+ if(!req.query.id){
+ res.status(404);
+ return;
+ }
+
+
+ });
+
+ app.listen(port);
+ console.log('Listening on: '+port);*/
+
+var source = request({
+    url: 'http://www.deon.pl/wiadomosci/biznes-gospodarka/art,4357,czy-gaz-na-slowacje-poplynie-przez-polske.html',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0'
     }
+}, function (error, response, body) {
 
+    if(response.statusCode==200){
+
+        var $ = cheerio.load(body);
+
+        var date = $('.aktualnosc .data')[0];
+
+        var re = /([0-9]{2}\.[0-9]{2}\.[0-9]{4}\s+[0-9]{2}:[0-9]{2})/g;
+
+        var parsedDate = re.exec(cheerio(date).toString());
+
+        console.log(date[0]);
+
+    }
 
 });
 
-app.listen(port);
-console.log('Listening on: '+port);*/
+return;
 
 var source = request({
     url: 'http://deon.pl/rss/pl/29.xml',
-    headers:
-    {
+    headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0'
     }
-}, function(error, response, body){
+}, function (error, response, body) {
 
-    if(response.statusCode==200){
+    if (response.statusCode == 200) {
 
         var $ = cheerio.load(body, {
             xmlMode: true
         });
 
-        var contents = $('content');
+        var contents = $('item');
 
-        contents.each(function(i){
-            var content = contents.eq(i).html();
+        contents.each(function (i) {
+            var link = cheerio('guid', contents[i]).toString();
 
-            content = content.replace(/<!\[CDATA\[([^\]]+)\]\]>/ig, "$1");
+            cheerio(contents[i]).append('<dupa />');
 
-            var sub = cheerio.load(content);
-            var subs = sub('a');
-
-
+            if (i == contents.length - 1) {
+                console.log($.toString());
+            }
         });
 
 
-        res.header("Content-Type", "application/rss+xml");
-        res.status(200).send($.html());
+        //res.header("Content-Type", "application/rss+xml");
+        //res.status(200).send($.html());
 
-    }else{
-        res.status(response.statusCode);
+    } else {
+        //res.status(response.statusCode);
     }
 
 });
